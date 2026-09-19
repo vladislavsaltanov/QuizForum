b49b77e0758a3a279e8a1ac7f7efe4a2974d4983
Владислав Салтанов <saltanov@sfedu.ru>
2026-09-19 11:33:11 +0300

    ci(system-test): scaffold test/system and install Chrome in workflow

---
+++ test/system/sign_in_flow_test.rb
@@ -0,0 +1,13 @@
+require "application_system_test_case"
+
+class SignInFlowTest < ApplicationSystemTestCase
+  test "signing in with valid credentials" do
+    visit new_session_url
+
+    fill_in "email", with: "one@example.com"
+    fill_in "password", with: "password"
+    click_on "Sign in"
+
+    assert_current_path root_path
+  end
+end
